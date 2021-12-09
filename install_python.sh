5da3c95e5a513e65a90a1f98f7114567785b885f
Maximilian Böther <maxi+git@boether.de>
2021-12-09 18:26:44 +0100

    add python script

---
+++ install_python.sh
@@ -0,0 +1,131 @@
+PYTHON_VERSION=3.7.7
+PYTHON_MAJOR=3
+MAKE_FLAGS="-j16"
+
+INSTALLDIR=/scratch/maximilian.boether/opt/python-${PYTHON_VERSION}
+BUILDDIR=/scratch/maximilian.boether/tmp/python-${PYTHON_VERSION}_build
+SOURCEDIR=/scratch/maximilian.boether/tmp/python-${PYTHON_VERSION}_source
+TARDIR=/scratch/maximilian.boether/tmp/python-${PYTHON_VERSION}_tar
+
+# Set script to abort on any command that results an error status
+trap '__abort' 0
+set -e
+
+__wget()
+{
+    urlroot=$1; shift
+    tarfile=$1; shift
+
+    if [ ! -e "$TARDIR/$tarfile" ]; then
+        wget --verbose ${urlroot}/$tarfile --directory-prefix="$TARDIR"
+    else
+        echo "already downloaded: $tarfile  '$TARDIR/$tarfile'"
+    fi
+}
+
+__untar()
+{
+    dir="$1";
+    file="$2"
+    case $file in
+        *xz)
+            tar xJ -C "$dir" -f "$file"
+            ;;
+        *bz2)
+            tar xj -C "$dir" -f "$file"
+            ;;
+        *gz)
+            tar xz -C "$dir" -f "$file"
+            ;;
+        *)
+            __die "don't know how to unzip $file"
+            ;;
+    esac
+}
+
+
+__abort()
+{
+        cat <<EOF
+***************
+*** ABORTED ***
+***************
+An error occurred. Exiting...
+EOF
+        exit 1
+}
+
+__die()
+{
+    echo $*
+    exit 1
+}
+
+#======================================================================
+# Directory creation
+#======================================================================
+# ensure workspace directories don't already exist
+for d in  "$BUILDDIR" "$SOURCEDIR" ; do
+    if [ -d  "$d" ]; then
+        __die "directory already exists - please remove and try again: $d"
+    fi
+done
+
+for d in "$INSTALLDIR" "$BUILDDIR" "$SOURCEDIR" "$TARDIR" ;
+do
+    test  -d "$d" || mkdir --verbose -p $d
+done
+
+#======================================================================
+# Download source code
+#======================================================================
+
+__wget https://www.python.org/ftp/python/${PYTHON_VERSION} Python-${PYTHON_VERSION}.tgz
+
+# Check tarfiles are found, if not found, dont proceed
+if [ ! -f "$TARDIR/Python-${PYTHON_VERSION}.tgz" ]; then
+    __die tarfile not found: $TARDIR/$f
+fi
+
+
+#======================================================================
+# Unpack source tarfiles
+#======================================================================
+__untar  "$SOURCEDIR"  "$TARDIR/Python-${PYTHON_VERSION}.tgz"
+
+
+#======================================================================
+# Configure
+#======================================================================
+cd "${BUILDDIR}"
+
+./configure \
+    --prefix=$INSTALLDIR \
+    --enable-shared \
+    --enable-ipv6 \
+    LDFLAGS=-Wl,-rpath=$INSTALLDIR/lib,--disable-new-dtags
+
+#======================================================================
+# Compiling and installing
+#======================================================================
+
+cd "$BUILDDIR"
+make $MAKE_FLAGS
+make $MAKE_FLAGS install
+
+#======================================================================
+# Post build
+#======================================================================
+
+# Create a shell script that users can source to bring gcc into shell
+# environment
+cat << EOF > ${INSTALLDIR}/activate
+# source this script to bring python ${PYTHON_VERSION} into your environment
+export PATH=${INSTALLDIR}/bin:\$PATH
+EOF
+
+trap : 0
+
+#end
+
+
